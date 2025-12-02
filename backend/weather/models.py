67d01f1aa4db7c27b709e95ef68fba975a643c1c
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-12-02 12:38:01 +0100

    feat: modificación atributos de modelos en models.py

--- backend/weather/models.py
+++ backend/weather/models.py
@@ -1,14 +1,19 @@
 from django.db import models
-
+from django.utils import timezone
 
 class City(models.Model):
+    # Modelo para almacenar ciudades en ubicaciones geográficas
     name = models.CharField(max_length=100)
+    latitud = models.FloatField()
+    longitud = models.FloatField()
+    altitud = models.FloatField(null=True, blank=True, help_text="Metros sobre el nivel del mar")
 
     def __str__(self):
         return self.name
 
 
 class WeatherObservation(models.Model):
+    # Modelo para observar el cambio climático
     city = models.ForeignKey(City, on_delete=models.CASCADE, related_name="observations")
     timestamp = models.DateTimeField()
     temperature = models.FloatField()
--- backend/weather/urls.py
+++ backend/weather/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import ProphetForecastView
 
+app_name = 'weather'
+
 urlpatterns = [
     path("api/forecast/prophet/", ProphetForecastView.as_view(), name="prophet-forecast"),
 ]
