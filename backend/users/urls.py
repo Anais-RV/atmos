3d3892888742cbc11ccad37bb340b745f8faf65b
WiZiSergio <wizisergio@gmail.com>
2025-12-05 10:18:47 +0100

    Add two lines in urls.py

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RegisterView, MeView, AdminOnlyView, ProfileView
+from .views import RegisterView, MeView, AdminOnlyView, ProfileView, SuperuserOnlyView, PublicView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("me/", MeView.as_view(), name="me"),
     path("profile/", ProfileView.as_view(), name="profile"),
     path("admin-only/", AdminOnlyView.as_view(), name="admin-only"),
+    path("superuser-only/", SuperuserOnlyView.as_view(), name="superuser-only"),
+    path("public/", PublicView.as_view(), name="public"),
 ]
