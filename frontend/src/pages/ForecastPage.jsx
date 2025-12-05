6665edaebab3b5493f60148da0a220ad7917ab1d
Iván Calero <ivanicaleroo@gmail.com>
2025-12-05 12:11:24 +0100

    feat/Initial forecast (#31)
    * feat/Initial forecast
    
    * Implement ForecastPage and ForecastExtendedPage, and navigate to the extended forecast when clicking on the forecast placeholder
    
    ---------
    
    Co-authored-by: Anaïs <anaisrv@gmail.com>

---
+++ frontend/src/pages/ForecastPage.jsx
@@ -0,0 +1,49 @@
+// frontend/src/pages/ForecastPage.jsx
+
+import BasePageLayout from "../components/layout/BasePageLayout";
+import { getTemperatureColor } from "../styles/temperatureColors";
+import Forecast from "../components/forecast/Forecast";
+import "../styles/forecast.css";
+
+function ForecastPage() {
+  const temperatureC = 7; // igual que Dashboard/Charts para mantener coherencia
+  const containerColor = getTemperatureColor(temperatureC);
+
+  return (
+    <BasePageLayout
+      title=""
+      description=""
+      containerColor={containerColor}
+    >
+      {/* MISMO LAYOUT QUE DASHBOARD (dos center-card) */}
+      <section className="dashboard-center">
+        {/* Tarjeta superior */}
+        <section className="center-card center-card-top">
+          <h2 className="center-card-title">FORECAST</h2>
+          <p className="center-card-text">
+            Consulta la predicción meteorológica de PROPET.
+          </p>
+
+          {/* Zona donde irán las gráficas de predicción */}
+          <div className="forecast-main-viewport">
+            <Forecast />
+          </div>
+        </section>
+
+        {/* Tarjeta inferior */}
+        <section className="center-card center-card-bottom">
+          <h2 className="center-card-title">Forecast summary</h2>
+          <p className="center-card-text">
+            Predicciones de días próximos
+          </p>
+
+          <div className="forecast-summary-grid">
+            {/* Aquí irán las tarjetas de resumen de la predicción */}
+          </div>
+        </section>
+      </section>
+    </BasePageLayout>
+  );
+}
+
+export default ForecastPage;
