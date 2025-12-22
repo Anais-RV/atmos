1bb9efa88ae7f0b85febeb31900fd2eab0e27cda
WiZiSergio <wizisergio@gmail.com>
2025-12-22 13:26:24 +0100

    Added ALLOWED_HOSTS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-v5m=4q4)lopw%dfij@%sz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver', '*']
 
 
 # Application definition
